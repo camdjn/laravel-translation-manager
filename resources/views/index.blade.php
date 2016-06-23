15f4ed994546fa9c41000ff5838e9fd407051b83
kammot <cam.dejardin@gmail.com>
2016-06-23 15:18:06 +0200

    change position + add parameters to empty button

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -138,8 +138,9 @@
                     @if(!$allTranslations->where('locale_id',$locale->id)->where('key',$keys->first()->key)->isEmpty() &&
                       !is_null($allTranslations->where('locale_id',$locale->id)->where('key',$keys->first()->key)->first()->value))
                       <textarea name="value" class="form-control" rows="3">{{$allTranslations->where('locale_id',$locale->id)->where('key', $keys->first()->key)->first()->value }}</textarea>
-                      <a class="btn btn-danger" href="{{ action('\camdjn\TranslationManager\Controller@getEmpty')}}"><span class="glyphicon glyphicon-trash"></span></a>
                       <input type="submit" value="edit" class="btn btn-primary">
+                      <a class="btn btn-danger" href="{{ action('\camdjn\TranslationManager\Controller@getEmpty', [$group, $locale, $key])}}"><span class="glyphicon glyphicon-trash"></span></a>
+
                     @else
                       <textarea name="value" class="form-control" rows="3" placeholder="empty"></textarea>
                       <input type="submit" value="add" class="btn btn-success">
